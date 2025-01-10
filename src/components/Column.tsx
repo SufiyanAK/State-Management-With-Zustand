218c9618b62ed57837207827d1e04f0fec693d05
SufiyanAK <sufiyankhattak14@gmail.com>
2025-01-10 18:29:18 +0500

    feat: implement task addition functionality with modal in Column component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,26 +1,68 @@
-import { FC, useMemo } from "react";
+import { FC, useMemo, useState } from "react";
 import Task from "./Task";
-import { useStore } from "../zustand/store";
+import { status, useStore } from "../zustand/store";
 
 interface ColumnProps {
-  state: string;
+  state: status;
 }
 
+const taskStatus: Array<status> = ["PLANNED", "ONGOING", "DONE"]
+
 const Column: FC<ColumnProps> = ({ state }) => {
+  const [text, setText] = useState<string>('')
+  const [status, setStatus] = useState<status>('PLANNED')
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   // This is a custom hook that filters tasks based on their status and it is the react solution
   const tasks = useStore(store => store.tasks)
 
   const filteredTasks = useMemo(() => tasks.filter(task => task.status === state)
     , [tasks, state])
 
-  console.log('filtered Tasks', filteredTasks)
+  const addTask = useStore(store => store.addTask)
+
+  const handleChange = (value: string) => { setText(value) }
+
+  const handleStatusChange = (value: status) => { setStatus(value) }
+
+  const resetFields = () => {
+    setText('')
+    setStatus('PLANNED')
+  }
   return (
     <div className="min-h-[20rem] w-[15rem] bg-gray-100 rounded-md p-4">
       <div className="flex justify-between mb-3">
         <p>{state}</p>
-        <button className="px-4 py-2 bg-white rounded hover:bg-green-100 hover:shadow-md duration-150">Add</button>
+        <button className="px-4 py-2 bg-white rounded hover:bg-green-100 hover:shadow-md duration-150"
+          onClick={() => setIsModalOpen(true)}>Add</button>
       </div>
-      {filteredTasks.map(task => <Task key={task.title} title={task.title} STATUS={task.status} />)}
+      {filteredTasks.map((task, index) => <Task key={`${task.title}-${index}`} title={task.title} STATUS={task.status} />)}
+
+      {/* Modal */}
+      <div className={`fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 duration-300 flex items-center justify-center ${isModalOpen ? 'block' : 'hidden'}`}>
+        <div className="bg-white p-4 rounded-md">
+          <div className=" flex flex-col mb-4">
+            <label htmlFor="text" className="text-lg">Task:</label>
+            <input className="border-2 rounded min-h-10 p-3" id="text" type="text" value={text} onChange={(e) => handleChange(e.target.value)} />
+          </div>
+          <div className=" flex flex-col mb-4">
+            <label htmlFor="status" className="text-lg">Status:</label>
+            <select className="border-2 rounded min-h-10 p-3" id="status" defaultValue="Please Select" value={status} onChange={(e) => handleStatusChange(e.target.value as status)}>
+              {
+                taskStatus.map((status, index) => <option key={`${status}-${index}`} value={status}>{status}</option>)
+              }
+            </select>
+          </div>
+          <div className="text-center">
+            <button
+              onClick={() => {
+                addTask(text, status)
+                setIsModalOpen(false)
+                resetFields()
+              }}
+              className="px-4 py-2 bg-gray-100 rounded hover:bg-green-100 hover:shadow-md duration-150">Add Task</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/zustand/store.ts
+++ src/zustand/store.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand"
 
-type status = "PLANNED" | "ONGOING" | "DONE";
+export type status = "PLANNED" | "ONGOING" | "DONE";
 
 export interface Task {
     title: string;
@@ -9,6 +9,7 @@ export interface Task {
 
 interface Store {
     tasks: Task[];
+    addTask(title: string, status: status): void;
 }
 
 export const useStore = create<Store>()((set) => ({
@@ -17,4 +18,5 @@ export const useStore = create<Store>()((set) => ({
         { title: "Task 2", status: "ONGOING" },
         { title: "Task 3", status: "DONE" },
     ],
+    addTask: (title, status) => set((state) => ({ tasks: [...state.tasks, { title, status }] }))
 }))
